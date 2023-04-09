ee8c4b922294d5921164dfe55aac6dfc2e792424
miladbeigi <milad.be@gmail.com>
2023-04-09 20:43:28 +0200

    feat: init server module

---
+++ server/src/test/kotlin/wa2/lab2/server/ServerApplicationTests.kt
@@ -0,0 +1,13 @@
+package wa2.lab2.server
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ServerApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
