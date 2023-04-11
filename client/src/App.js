5fe86990622de0816fc55058f6143ebe47725ef9
Shayan Taghinezhad Roudbaraki s301425 <s301425@studenti.polito.it>
2023-04-11 19:04:34 +0200

    Client get /API/products implemented.

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
+import {useEffect, useState} from "react";
 
 function App() {
+  const [products, setProducts] = useState([]);
+  useEffect(() => {
+    fetch('http://localhost:8080/API/products/')
+        .then((response) => response.json())
+        .then((data) => {
+          console.log(data);
+          setProducts(data);
+        })
+        .catch((err) => {
+          console.log(err.message);
+            setProducts(err.message)
+        });
+  }, []);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <p>Hello ReactJS!</p>
+        <div className="products-container">
+            {products}
+        </div>
     </div>
   );
 }
