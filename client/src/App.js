f153acd6e7541af00bdf4c888c944c6b4cbbe71c
miladbeigi <milad.be@gmail.com>
2023-04-12 00:00:21 +0200

    feat: add Get Products button

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,19 @@
 import './App.css';
-import {useEffect, useState} from "react";
+import {useState} from "react";
 import Button from '@mui/material/Button';
 import FolderList from "./components/List";
 import API from "./api/products";
 
 function App() {
     const [products, setProducts] = useState([]);
-    useEffect(() => {
-        async function loadProducts() {
-            const loadedProducts = await API.getProducts();
-            setProducts(loadedProducts);
-        } loadProducts();
-    }, []);
+    const handleGetProducts = async () => {
+        await API.getProducts().then((res) => setProducts(res))
+    }
   return (
     <div className="App">
         <p>Hello ReactJS!</p>
         <div className="products-container">
-            <Button variant="contained">Get Products</Button>
+            <Button variant="contained" onClick={  () => {handleGetProducts().then()}  }>Get Products</Button>
             <FolderList products={products}></FolderList>
         </div>
     </div>
