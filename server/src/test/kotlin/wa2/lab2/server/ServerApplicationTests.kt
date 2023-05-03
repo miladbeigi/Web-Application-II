a9b3abac051cdf6022c5c0c615423605b79952ac
miladbeigi <milad.be@gmail.com>
2023-05-03 23:40:35 +0200

    feat: add tests

--- server/src/test/kotlin/wa2/lab2/server/ServerApplicationTests.kt
+++ server/src/test/kotlin/wa2/lab2/server/ServerApplicationTests.kt
@@ -19,10 +19,12 @@ import wa2.lab2.server.profiles.exceptions.ProfileNotFoundException
 import wa2.lab2.server.ticketing.TicketHistoryRepository
 import wa2.lab2.server.ticketing.TicketRepository
 import wa2.lab2.server.ticketing.TicketServiceImp
+import wa2.lab2.server.ticketing.TicketStatus
+import wa2.lab2.server.ticketing.exceptions.TicketExceptions
 
 @Testcontainers
-@SpringBootTest(webEnvironment=SpringBootTest.WebEnvironment.RANDOM_PORT)
-@AutoConfigureTestDatabase(replace= AutoConfigureTestDatabase.Replace.NONE)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 class ServerApplicationTests {
     companion object {
         @Container
@@ -51,47 +53,40 @@ class ServerApplicationTests {
     @Autowired
     lateinit var ticketHistoryRepository: TicketHistoryRepository
 
+    @Autowired
+    lateinit var ticketService: TicketServiceImp
+
+    @Autowired
+    lateinit var profileService: ProfileServiceImp
+
+    @Autowired
+    lateinit var productService: ProductServiceImp
+
     @Test
     fun contextLoads() {
     }
 
     @Test
     fun createProfile() {
-        val profileService = ProfileServiceImp(profileRepository)
         profileService.createProfile(
-            email = "milad@gmail.com",
-            name = "M",
-            lastname = "B")
+            email = "milad@gmail.com", name = "M", lastname = "B"
+        )
     }
+
     @Test
     fun createProduct() {
-        val productService = ProductServiceImp(productRepository)
         productService.createProduct(
-            ean = "1234567890122",
-            name = "Test product",
-            brand = "IPhone 6")
+            ean = "1234567890122", name = "Test product", brand = "IPhone 6"
+        )
     }
 
     @Test
     fun createTicket() {
-
-        val productService = ProductServiceImp(productRepository)
         productService.createProduct(
-            ean = "1234567890123",
-            name = "IPhone 12",
-            brand = "Apple")
-
-        val profileService = ProfileServiceImp(profileRepository)
+            ean = "1234567890123", name = "IPhone 12", brand = "Apple"
+        )
         profileService.createProfile(
-            email = "milad.be@gmail.com",
-            name = "M",
-            lastname = "B")
-
-        val ticketService = TicketServiceImp(
-            ticketRepository,
-            ticketHistoryRepository,
-            productService,
-            profileService
+            email = "milad.be@gmail.com", name = "M", lastname = "B"
         )
 
         assertThrows<ProfileNotFoundException> {
@@ -99,7 +94,8 @@ class ServerApplicationTests {
                 title = "Test ticket",
                 description = "Test ticket description",
                 productId = "1234567890123",
-                profileId = "")
+                profileId = ""
+            )
         }
 
         assertThrows<ProductNotFoundException> {
@@ -107,14 +103,16 @@ class ServerApplicationTests {
                 title = "Test ticket",
                 description = "Test ticket description",
                 productId = "",
-                profileId = "milad.be@gmail.com")
+                profileId = "milad.be@gmail.com"
+            )
         }
 
         val ticketId: Long? = ticketService.createTicket(
             title = "Test ticket",
             description = "Test ticket description",
             productId = "1234567890123",
-            profileId = "milad.be@gmail.com")
+            profileId = "milad.be@gmail.com"
+        )
 
         assert(ticketId != null)
     }
@@ -122,20 +120,60 @@ class ServerApplicationTests {
     // TODO: Add tests for the following functions
     @Test
     fun startTicket() {
-        assert(true)
+        productService.createProduct(
+            ean = "1234567890113", name = "IPhone 12", brand = "Apple"
+        )
+
+        profileService.createProfile(
+            email = "m@gmail.com", name = "M", lastname = "B"
+        )
+
+        val ticketId: Long? = ticketService.createTicket(
+            title = "Test ticket",
+            description = "Test ticket description",
+            productId = "1234567890113",
+            profileId = "m@gmail.com"
+        )
+
+        assertThrows<TicketExceptions> { ticketService.startTicket(ticketId.toString()) }
+
+        ticketService.stopTicket(ticketId.toString())
+        ticketService.startTicket(ticketId.toString())
+
+        assert(ticketRepository.findById(ticketId!!).get().status == TicketStatus.InProgress)
     }
+
     @Test
     fun stopTicket() {
-        assert(true)
+        productService.createProduct(
+            ean = "1234567890114", name = "IPhone 12", brand = "Apple"
+        )
+        profileService.createProfile(
+            email = "mb@gmail.com", name = "M", lastname = "B"
+        )
+
+        val ticketId: Long? = ticketService.createTicket(
+            title = "Test ticket",
+            description = "Test ticket description",
+            productId = "1234567890114",
+            profileId = "mb@gmail.com"
+        )
+
+        ticketService.stopTicket(ticketId.toString())
+        assertThrows<TicketExceptions> { ticketService.stopTicket(ticketId.toString()) }
+        assert(ticketRepository.findById(ticketId!!).get().status == TicketStatus.Open)
     }
+
     @Test
     fun closeTicket() {
         assert(true)
     }
+
     @Test
     fun reopenTicket() {
         assert(true)
     }
+
     @Test
     fun resolveTicket() {
         assert(true)
